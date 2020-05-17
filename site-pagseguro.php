5e26d80d4e4fe21979f05e5f3c01c7421958272b
Renato Oliveira <renatooliveira@MacBook-Pro-de-Renato.local>
2020-05-17 17:05:24 -0300

    Ïnicio do projeto Pagseguro com Ecommerce

---
+++ site-pagseguro.php
@@ -0,0 +1,25 @@
+<?php 
+
+use \Hcode\Page;
+use \Hcode\Model\User;
+use \GuzzleHttp\Client;
+use \Hcode\PagSeguro\Config;
+
+$app->get('/payament/pagseguro', function() {
+
+    echo "Renato: </br>";
+    $client = new Client();
+    $response = $client->request('POST', Config::getUrlSessions() . "?" . http_build_query(Config::getAuthentication()), 
+            [
+                'verify'=>false
+            ] 
+    );
+    
+    echo "Oliveira: </br>";
+
+    // echo $response->getStatusCode(); // 200
+    // echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+     echo $response->getBody()->getContents(); // '{"id": 1420053, "name": "guzzle", ...}'
+    exit;
+
+});
---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Hcode\PagSeguro;
+
+class Config {//class
+
+    const SANDBOX = true;
+
+    const SANDBOX_EMAIL = "desenvolvimento@witcare.com.br";
+    const PRODUCTION_EMAIL = "desenvolvimento@witcare.com.br";
+    
+    const SANDBOX_TOKEN = "BE25B897CD2849CABB5CDC483679BC6D";
+    const PRODUCTION_TOKEN = "521e9df8-8c7d-4c22-934f-b002168afc1dc0a27a9d4e95a9d95bb13164b4bb1974bfd0-017a-4107-a628-3698660d9f7f";
+
+    const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
+    const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
+
+    public static function getAuthentication()
+    {//func 1
+
+        if( Config::SANDBOX === true )
+        {//if 1
+
+            return [
+                "email"=>Config::SANDBOX_EMAIL,
+                "token"=>Config::SANDBOX_TOKEN
+            ];
+
+        }//if 1
+          else{//else 1
+            return [
+                "email"=>Config::PRODUCTION_EMAIL,
+                "token"=>Config::PRODUCTION_TOKEN
+            ];
+          }//else 1
+
+    }//func 1
+
+    public static function getUrlSessions():string
+    {//func 2
+        
+        return ( Config::SANDBOX === true ) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
+
+    }//func 2
+}//class
+
+?>
+
+
+
